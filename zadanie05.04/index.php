4e7e43ab007f29b289409fb1c5660ccc29212319
Czeechu <112950394+Czeechu@users.noreply.github.com>
2024-04-05 16:20:26 +0200

    Add files via upload

---
+++ zadanie05.04/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona główna</title>
+    
+</head>
+<body>
+    <h2>Zaloguj sie do panelu</h2>
+    <form action="login.php" method="post">
+        Login:<input type="text" name="username" required>
+        <br>
+        Haslo:<input type="password" name="password" required>
+        <br>
+
+        <input type="submit" value="Zaloguj">
+    </form>
+    <script src="plik.php">
+        
+    </script>
+</body>
+</html>
